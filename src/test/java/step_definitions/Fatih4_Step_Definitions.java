0b81944a87957310cf75e59afcbe73d673d26351
[Volodymyr Bondarenko] <[RedefinePL@gmail.com]>
2018-08-27 15:05:24 -0400

    # WARNING: head commit changed in the meantime
    DBUtils file added

--- src/test/java/step_definitions/Fatih4_Step_Definitions.java
+++ src/test/java/step_definitions/Fatih4_Step_Definitions.java
@@ -5,6 +5,11 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class Fatih4_Step_Definitions {
 	
 	@Given("As a qa_user I logged into cybertekbnb database")
